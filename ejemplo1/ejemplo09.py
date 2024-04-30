ecb73f55aa8a2fe9f52138657a52e32d2d9408dc
uuunin <rdcarrion3@utpl.edu.ec>
2024-04-30 10:19:20 -0500

    Generación de archivos unicos a partir del ganador del torneo, fecha y nombre del torneo

--- ejemplo1/ejemplo09.py
+++ ejemplo1/ejemplo09.py
@@ -3,7 +3,8 @@
 """
 
 
-archivo = open("data/atp_tennis.csv", "r")
+archivo = open("ejemplo1/data/atp_tennis.csv", "r")
 
 lineas = archivo.readlines()
+print(lineas[0])
 print(len(lineas))
--- ejemplo1/ejemplo10.py
+++ ejemplo1/ejemplo10.py
@@ -3,20 +3,21 @@
 """
 
 
-archivo = open("data/atp_tennis.csv", "r")
+archivo = open("ejemplo1/data/atp_tennis.csv", "r")
 
 lineas = archivo.readlines()
 print(len(lineas))
 
 
 lineas = [l.split("|") for l in lineas]
-contador = 0
+contador = 1
 for x in lineas:
     
     cadena = """<b>Torneo:</b> %s<br> <b>Ganador:</b> %s""" % (x[0], x[9])
     print(cadena)
+    print(contador)
 
-    archivo_generado = open("data/%s%d.html" % (x[9], contador), "w")
+    archivo_generado = open("ejemplo1/winners_of_tournaments/%s-%s-%s.html" % (x[9], x[0], x[1]), "w")
     archivo_generado.writelines("%s\n" % (cadena))
     archivo_generado.close()
     contador+=1
